dae237908ab91b545c5451c225c95162c1d0da04
David Bailey <dabailey@davidabailey.com>
2016-04-17 13:50:42 -0700

    deleted:    .newsbeuter/config 	deleted:    .newsbeuter/urls 	deleted:    .ssh/config 	deleted:    .ssh/id_rsa 	deleted:    .ssh/id_rsa.pub 	deleted:    bin/news.sh 	deleted:    bin/scores.py 	deleted:    bin/stocks.py 	deleted:    bin/times.sh 	deleted:    bin/weather.py

--- bin/scores.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import os
-
-teams= ["usc+trojans+basketball", "usc+trojans+football", "san+francisco+49ers", "los+angeles+lakers", "san+jose+sharks", "chelsea"]
-for team in teams:
-	print team
-	os.system('lynx -nonumbers -dump http://www.google.com/search?q=' + team + '| grep "Last game"')
-	os.system('lynx -nonumbers -dump http://www.google.com/search?q=' + team + '| grep "Next game"')
--- bin/times.sh
+++
@@ -1,6 +0,0 @@
-TZ=America/Los_Angeles	date "+Los Angeles: %H:%M:%S  %A %B %d %Y"
-TZ=America/New_York 	date "+New York:    %H:%M:%S  %A %B %d %Y"
-TZ=Europe/London 	date "+London:      %H:%M:%S  %A %B %d %Y"
-TZ=Europe/Paris 	date "+Paris:       %H:%M:%S  %A %B %d %Y"
-TZ=Asia/Hong_Kong 	date "+Hong Kong:   %H:%M:%S  %A %B %d %Y"
-TZ=Asia/Tokyo 		date "+Tokyo:       %H:%M:%S  %A %B %d %Y"
